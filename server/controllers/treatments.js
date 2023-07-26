b3af2124b2de812b41913c1466fdb2f7ac0150f4
Amir Nakhaei <6696894+amirrr@users.noreply.github.com>
2023-07-26 21:08:20 +0200

    changed website title and added instagram

--- server/controllers/treatments.js
+++ server/controllers/treatments.js
@@ -79,10 +79,10 @@ const readTreatments = async (req, res, next) => {
 
 const chooseTreatment = async (req_param) => {
   const treatmentIds =
-    req_param.query.source === "facebook"
-      ? [4, 5, 6, 7, 8, 9, 10, 11]
-      : [1, 2, 3];
-
+    source === "facebook" || source === "instagram"
+      ? [1, 2, 3]
+      : [4, 5, 6, 7, 8, 9, 10, 11];
+      
   const treatmentCount = await treatments
     .findAll({
       attributes: [
