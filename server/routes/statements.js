b5223775cd34302d0641c1262c6c754fe42282ad
Amir Nakhaei <6696894+amirrr@users.noreply.github.com>
2023-11-25 22:16:27 +0100

    draft for statement saving backend

--- server/routes/statements.js
+++ server/routes/statements.js
@@ -4,5 +4,7 @@ const controller = require("../controllers/statements.js");
 router.get("/", controller.baseStatements);
 router.get("/next", controller.next);
 router.get("/byid/:statementId", controller.statementById);
+router.post("/submit-statement", controller.saveSubmitedStatements);
+
 
 module.exports = router;
