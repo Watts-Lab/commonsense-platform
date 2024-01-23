e40ed8f3c77161685da9b6094d2365018c6f68d6
amirrr <6696894+amirrr@users.noreply.github.com>
2024-01-23 22:44:47 +0100

    Remove unused imports in Footer component

--- client/src/partials/Footer.tsx
+++ client/src/partials/Footer.tsx
@@ -1,8 +1,3 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import About from "../pages/About";
-import { Popover } from "@headlessui/react";
-import WidgetForm from "../components/Feedback/WidgetForm";
 import ReportIssue from "../components/Feedback/ReportIssue";
 
 function Footer() {
