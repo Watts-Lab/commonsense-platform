9b2852485698cb9c2f120987e0b757c5ca705c32
amirrr <6696894+amirrr@users.noreply.github.com>
2024-04-12 19:55:41 +0200

    Add darkmode

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-  darkMode: "class",
 };
