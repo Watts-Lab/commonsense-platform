3a94963c72d319d0b14a4a4e0e5df2c753696641
amirrr <6696894+amirrr@users.noreply.github.com>
2024-06-11 00:35:45 +0200

    fix survey test

--- client/cypress/e2e/1-functionality/formfill.cy.js
+++ client/cypress/e2e/1-functionality/formfill.cy.js
@@ -32,7 +32,7 @@ describe("fill out the survay", () => {
         .check({ force: true })
         .should("be.checked");
       // cy.get(
-        
+
       //   "input[type='radio'][id*=\"question6-I think most people have good judgement with regard to this topic\"]"
       // )
       //   .check({ force: true })
@@ -40,11 +40,68 @@ describe("fill out the survay", () => {
 
       // Check if it's the 15th iteration and select the "Finish" button
       if (i === numberOfIterations - 1) {
-        cy.contains("button", "Finish").click({ force: true });
+        cy.contains("button", "Next →").click({ force: true });
       } else {
         // Otherwise, click the "Next" button (modify the selector as needed)
         cy.contains("button", "Next →").click({ force: true });
       }
     }
+
+    // Go to individual question pages
+
+    cy.get("input[type='number'][id*='sq_106i']").type("1");
+    cy.get("input[type='number'][id*='sq_107i']").type("1");
+    cy.get("input[type='number'][id*='sq_108i']").type("1");
+    cy.get("input[type='number'][id*='sq_109i']").type("1");
+    cy.get("input[type='number'][id*='sq_110i']").type("1");
+    cy.get("input[type='number'][id*='sq_111i']").type("1");
+
+    cy.get("input[type='button'][value='Complete']").click();
+
+    // Go through RME questions
+
+    const list_of_ids = [
+      "sq_133i_0",
+      "sq_135i_0",
+      "sq_137i_0",
+      "sq_139i_0",
+      "sq_141i_0",
+      "sq_143i_0",
+      "sq_145i_0",
+      "sq_147i_0",
+      "sq_149i_0",
+    ];
+
+    list_of_ids.forEach((id) => {
+      cy.get(`input[type='radio'][id*='${id}']`)
+        .check({ force: true })
+        .should("be.checked");
+      cy.get("input[type='button'][value='Next']").click();
+    });
+
+    // Go through the last page
+    cy.get(`input[type='radio'][id*='sq_151i_0']`)
+      .check({ force: true })
+      .should("be.checked");
+    cy.get("input[type='button'][value='Complete']").click();
+
+    // Fill the demographic form
+    cy.get("input[type='text'][id*='sq_169i']").type("1990");
+    cy.get("input[type='radio'][id*='sq_170i_2']")
+      .check({ force: true })
+      .should("be.checked");
+
+    cy.get("input[type='text'][id*='sq_173i_0']").type("English\n");
+
+    cy.get("input[type='radio'][id*='sq_175i_0']")
+      .check({ force: true })
+      .should("be.checked");
+
+    cy.get("input[type='radio'][id*='sq_176i_0']")
+      .check({ force: true })
+      .should("be.checked");
+
+    cy.get("input[type='text'][id*='sq_178i_0']").type("Algeria\n");
+    cy.get("input[type='button'][value='Complete']").click();
   });
 });
