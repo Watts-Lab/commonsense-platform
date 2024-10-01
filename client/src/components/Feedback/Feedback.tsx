a742e774dbb8a19735e4798ef3fdf356fae0d3ed
amirrr <6696894+amirrr@users.noreply.github.com>
2024-10-01 15:12:40 +0200

    Refactor Feedback component layout and styling

--- client/src/components/Feedback/Feedback.tsx
+++ client/src/components/Feedback/Feedback.tsx
@@ -1,22 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Popover } from "@headlessui/react";
 import ChatIcon from "./ChatIcon";
-
 import WidgetForm from "./WidgetForm";
 
 function Feedback() {
   return (
     <>
-      <Popover className="fixed bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+      <Popover className="lg:fixed bottom-0 left-0 w-full flex justify-center items-center md:bottom-8 md:right-2 md:w-auto md:left-auto md:justify-end">
         <Popover.Panel>
           <WidgetForm />
         </Popover.Panel>
-        <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-slate-600 flex items-center group">
-          {/* create a group */}
+        <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-slate-600 flex items-center group mb-4 md:mb-0">
           <ChatIcon />
-          <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-            {/* on hover increase width */}
-            {/* any element that overflows the max-width will be hidden */}
+          <span className="max-w-0 md:overflow-hidden md:group-hover:max-w-xs md:transition-all duration-500 ease-linear">
             <span className="pl-2"></span>
             Feedback
           </span>
