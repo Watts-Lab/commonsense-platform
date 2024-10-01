802fc891e521ffe798c6a13a82f23c1a90121616
amirrr <6696894+amirrr@users.noreply.github.com>
2024-10-02 00:21:10 +0200

    return button to navbar

--- client/src/partials/NavBar.tsx
+++ client/src/partials/NavBar.tsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import { useAppSelector } from "../redux/hooks";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 import ConsentModal from "../components/ConsentModal";
 import LocaleSwitcher from "../components/LocaleSwitcher";
 
 import Icon from "../images/Light-mode.svg";
-import '../css/styles.css';
+import "../css/styles.css";
 
 const Navbar: React.FC = () => {
   const loggedIn = useAppSelector((state) => state.login.loggedIn);
@@ -56,16 +56,16 @@ const Navbar: React.FC = () => {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52 text-gray-900"
             >
               <li className="mb-2">
-                <a href="/people">{t('navbar.people')}</a>
+                <a href="/people">{t("navbar.people")}</a>
               </li>
               <li className="mb-2">
-                <a href="/research">{t('navbar.research')}</a>
+                <a href="/research">{t("navbar.research")}</a>
               </li>
               <li className="mb-2">
                 {!loggedIn ? (
-                  <a href="/signin">{t('navbar.signin')}</a>
+                  <a href="/signin">{t("navbar.signin")}</a>
                 ) : (
-                  <a href="/dashboard">{t('navbar.dashboard')}</a>
+                  <a href="/dashboard">{t("navbar.dashboard")}</a>
                 )}
               </li>
             </ul>
@@ -75,7 +75,7 @@ const Navbar: React.FC = () => {
             <img className="h-9 block" src={Icon} alt="Logo" />
             <span className="text-lg font-bold inline-block ml-2">
               {/* The common sense project */}
-              {t('navbar.commonsense')}
+              {t("navbar.commonsense")}
             </span>
           </Link>
         </div>
@@ -84,20 +84,20 @@ const Navbar: React.FC = () => {
             <li className="mb-2">
               <a href="/people" className="button-long-text">
                 {/* People */}
-                {t('navbar.people')}
+                {t("navbar.people")}
               </a>
             </li>
             <li className="mb-2">
               <a href="/research" className="button-long-text">
                 {/* Research */}
-                {t('navbar.research')}
+                {t("navbar.research")}
               </a>
             </li>
             <li className="mb-2">
               {!loggedIn ? (
                 <a href="/signin" className="button-long-text">
                   {/* Signin */}
-                  {t('navbar.signin')}
+                  {t("navbar.signin")}
                 </a>
               ) : (
                 <>
@@ -107,7 +107,7 @@ const Navbar: React.FC = () => {
                     onClick={() => setMenuOpen(false)}
                   >
                     {/* Dashboard */}
-                    {t('navbar.dashboard')}
+                    {t("navbar.dashboard")}
                   </a>
                 </>
               )}
@@ -116,7 +116,10 @@ const Navbar: React.FC = () => {
         </div>
 
         <div className="navbar-end flex items-center px-2 lg:px-4">
-          {/* <ConsentModal buttonText={t('navbar.participate →')} buttonClass="btn" /> */}
+          <ConsentModal
+            buttonText={t("navbar.participate →")}
+            buttonClass="btn"
+          />
           <div className="ml-4">
             <LocaleSwitcher />
           </div>
