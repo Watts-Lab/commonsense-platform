b676f5f93efb1d3c3c71cc666fea7c93d70aa25c
amirrr <6696894+amirrr@users.noreply.github.com>
2024-10-01 12:06:50 +0200

    fix bottom widget

--- client/src/pages/SurveyPage.tsx
+++ client/src/pages/SurveyPage.tsx
@@ -7,10 +7,10 @@ import DarkModeSwitcher from "../components/DarkModeSwitch";
 const SurveyPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <main className="p-3 max-w-3xl w-full pb-14">
+      <main className="p-3 max-w-3xl w-full pb-32">
         <Layout />
         <Feedback />
-        <DarkModeSwitcher />
+        {/* <DarkModeSwitcher /> */}
       </main>
     </div>
   );
