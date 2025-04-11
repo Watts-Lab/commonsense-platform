fcc59182dc8b1467aed0f53b67874ab562913e99
amirrr <6696894+amirrr@users.noreply.github.com>
2025-04-11 08:56:27 +0200

    fix: update session cookie maxAge to prevent expiration

--- server/config/sessionConfig.js
+++ server/config/sessionConfig.js
@@ -7,7 +7,7 @@ module.exports = (session, sessionStore) => {
     store: sessionStore,
     cookie: {
       path: "/",
-      maxAge: 1000 * 60 * 60 * 1, // 1 hour
+      maxAge: null, // Session will not expire based on time
       httpOnly: true,
       sameSite: 'None',
       secure: true, // remove in production
